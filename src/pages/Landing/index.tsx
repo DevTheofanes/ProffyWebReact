a023bd64006538a8b8773c15c2cd85da258f2507
DevTheofanes <devtheofanes@gmail.com>
2020-08-06 17:42:02 -0300

    Rota create Teacher

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -19,6 +19,7 @@ function Landing() {
   useEffect(() => {
     api.get("/connections").then((response) => {
       const { total } = response.data;
+
       setTotalConnections(total);
     });
   }, []);
